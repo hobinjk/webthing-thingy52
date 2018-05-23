f17ad6544912932f31c151a3afa122dafd02164d
Michael Stegeman <michael@stegeman.me>
2018-05-23 09:19:41 -0600

    Several fixes: * Update to latest webthing API * Fix color and luminosity * Add ledColor property because the LED and the color sensor are   separate devices * Fix events

--- server.js
+++ server.js
@@ -4,162 +4,245 @@
  * exposing the Thingy:52 as a Web Thing
  */
 
-const {Action, Event, Property, Thing, Value, WebThingServer} = require('webthing');
-const uuidv4 = require('uuid/v4');
-var Thingy = require('thingy52');
-var Color = require('color');
+const {
+  Event,
+  Property,
+  SingleThing,
+  Thing,
+  Value,
+  WebThingServer,
+} = require('webthing');
+const Thingy = require('thingy52');
+const Color = require('color');
 
-console.log('Reading Thingy environment sensors!');
-
+console.log('Scanning for Thingy:52');
 
 class ButtonEvent extends Event {
   constructor(thing, state) {
-    super(thing, state, 'Button' + state);
+    super(thing, `button${state}`);
   }
-}
-
-class LedColorProperty extends Property {
-  constructor(thing, thingy) {
-    super(thing, 'color', new Value('#00f', () => {}), {type: 'string'});
-    this.thingy = thingy;
-  }
-
 }
 
 function run_server(thingy) {
-  console.log('Discovered: ' + thingy);
+  console.log('Discovered:', thingy);
 
-  thingy.on('disconnect', function() {
+  thingy.on('disconnect', () => {
     console.log('Disconnected!');
   });
 
-  const thing = new Thing('WoT Thingy', 'thing', 'A WoT-connected Thingy:52');
+  const thing = new Thing('thingy52', 'thing', 'A WoT-connected Thingy:52');
 
-  let temperatureProperty = new Property(thing,
-                                         'temperature',
-                                         new Value(0, () => {}),
-                                         {type: 'number',
-                                          unit: 'celsius',
-                                          description: 'An ambient temperature sensor'});
+  const temperatureProperty = new Property(
+    thing,
+    'temperature',
+    new Value(0),
+    {
+      type: 'number',
+      unit: 'celsius',
+      description: 'An ambient temperature sensor',
+    });
   thing.addProperty(temperatureProperty);
-  let pressureProperty = new Property(thing,
-                                      'pressure',
-                                      new Value(0, () => {}),
-                                      {type: 'number',
-                                       unit: 'hectopascal'});
+
+  const pressureProperty = new Property(
+    thing,
+    'pressure',
+    new Value(0),
+    {
+      type: 'number',
+      unit: 'hectopascal',
+    });
   thing.addProperty(pressureProperty);
-  let humidityProperty = new Property(thing,
-                                      'humidity',
-                                      new Value(0, () => {}),
-                                      {type: 'number',
-                                       unit: 'percent'});
+
+  const humidityProperty = new Property(
+    thing,
+    'humidity',
+    new Value(0),
+    {
+      type: 'number',
+      unit: 'percent',
+    });
   thing.addProperty(humidityProperty);
-  let gasProperty = new Property(thing,
-                                'gas',
-                                new Value({}, () => {}),
-                                {type: 'object'});
+
+  const gasProperty = new Property(
+    thing,
+    'gas',
+    new Value({}),
+    {
+      type: 'object',
+    });
   thing.addProperty(gasProperty);
 
-  let luminosityProperty = new Property(thing,
-                                        'luminosity',
-                                        new Value(0, () => {}),
-                                        {type: 'number',
-                                         unit: 'lux'});
+  const luminosityProperty = new Property(
+    thing,
+    'luminosity',
+    new Value(0),
+    {
+      type: 'number',
+      unit: 'lux',
+    });
   thing.addProperty(luminosityProperty);
 
-  let batteryLevelProperty = new Property(thing,
-                                          'battery',
-                                          new Value(0, () => {}),
-                                          {type: 'number',
-                                           unit: 'percent'});
+  const batteryLevelProperty = new Property(
+    thing,
+    'battery',
+    new Value(0),
+    {
+      type: 'number',
+      unit: 'percent',
+    });
   thing.addProperty(batteryLevelProperty);
 
-  let colorProperty = new Property(thing, 'color', new Value('#00f', (value) => {
-    let color = Color(value);
-    this.thingy.led_set(color.object(), function(error) {
-      console.log('LED color changed ' + ((error) ? error : ''));
+  const colorProperty = new Property(
+    thing,
+    'color',
+    new Value('#0000FF'),
+    {
+      type: 'string',
     });
-  }), {type: 'string'});
   thing.addProperty(colorProperty);
 
-  console.log('setup');
+  const ledColorProperty = new Property(
+    thing,
+    'ledColor',
+    new Value('#0000FF', (value) => {
+      const color = Color(value);
+      thingy.led_set(color.object(), (error) => {
+        if (error) {
+          console.log('Failed to set LED color:', error);
+        }
+      });
+    }),
+    {
+      type: 'string',
+    });
+  thing.addProperty(ledColorProperty);
 
-  thingy.connectAndSetUp(function(error) {
-    console.log('Connected! ' + error ? error : '');
+  thingy.connectAndSetUp((error) => {
+    if (error) {
+      console.log('Failed to connect:', error);
+      return;
+    }
 
-    thingy.on('temperatureNotif', temperatureProperty.setValue.bind(temperatureProperty));
-    thingy.on('pressureNotif', pressureProperty.setValue.bind(pressureProperty));
-    thingy.on('humidityNotif', humidityProperty.setValue.bind(humidityProperty));
-    thingy.on('gasNotif', gasProperty.setValue.bind(gasProperty));
-    thingy.on('colorNotif', value => {
-      let color = Color({r: value.red,
-                         g: value.green,
-                         b: value.blue});
-      colorProperty.setValue(value.hex());
-      luminosityProperty.setValue(value['clear']);
+    console.log('Connected!');
+
+    thingy.on('temperatureNotif', (value) => {
+      temperatureProperty.value.notifyOfExternalUpdate(value);
+    });
+    thingy.on('pressureNotif', (value) => {
+      pressureProperty.value.notifyOfExternalUpdate(value);
+    });
+    thingy.on('humidityNotif', (value) => {
+      humidityProperty.value.notifyOfExternalUpdate(value);
+    });
+    thingy.on('gasNotif', (value) => {
+      gasProperty.value.notifyOfExternalUpdate(value);
+    });
+    thingy.on('colorNotif', (value) => {
+      // eslint-disable-next-line max-len
+      // https://github.com/NordicPlayground/Nordic-Thingy52-Thingyjs/blob/master/js/ColorSensor.js
+      const r = value.red;
+      const g = value.green;
+      const b = value.blue;
+      const c = value.clear;
+      const rRatio = r / (r + g + b);
+      const gRatio = g / (r + g + b);
+      const bRatio = b / (r + g + b);
+      const clearAtBlack = 300;
+      const clearAtWhite = 400;
+      const clearDiff = clearAtWhite - clearAtBlack;
+      const clearNormalized = Math.max((c - clearAtBlack) / clearDiff, 0);
+
+      const red = Math.min(rRatio * 255.0 * 3 * clearNormalized, 255);
+      const green = Math.min(gRatio * 255.0 * 3 * clearNormalized, 255);
+      const blue = Math.min(bRatio * 255.0 * 3 * clearNormalized, 255);
+
+      const color = Color({
+        r: red.toFixed(0),
+        g: green.toFixed(0),
+        b: blue.toFixed(0),
+      });
+
+      colorProperty.value.notifyOfExternalUpdate(color.hex());
+      luminosityProperty.value.notifyOfExternalUpdate(value.clear);
+    });
+    thingy.on('batteryLevelChange', (value) => {
+      batteryLevelProperty.value.notifyOfExternalUpdate(value);
+    });
+    thingy.on('buttonNotif', (state) => {
+      thing.addEvent(new ButtonEvent(thing, state));
     });
 
-    thingy.on('batteryLevelChange', batteryLevelProperty.setValue.bind(batteryLevelProperty));
-
-    thingy.on('buttonNotif', state => thing.addEvent(new ButtonEvent(thing, state)));
-
-    thingy.temperature_interval_set(1000, function(error) {
+    thingy.temperature_interval_set(1000, (error) => {
       if (error) {
-        console.log('Temperature sensor configure! ' + error);
+        console.log('Failed to configure temperature sensor:', error);
       }
     });
-    thingy.pressure_interval_set(1000, function(error) {
+    thingy.pressure_interval_set(1000, (error) => {
       if (error) {
-        console.log('Pressure sensor configure! ' + error);
+        console.log('Failed to configure pressure sensor:', error);
       }
     });
-    thingy.humidity_interval_set(1000, function(error) {
+    thingy.humidity_interval_set(1000, (error) => {
       if (error) {
-        console.log('Humidity sensor configure! ' + error);
+        console.log('Failed to configure humidity sensor:', error);
       }
     });
-    thingy.color_interval_set(1000, function(error) {
+    thingy.color_interval_set(1000, (error) => {
       if (error) {
-        console.log('Color sensor configure! ' + error);
+        console.log('Failed to configure color sensor:': error);
       }
     });
-    thingy.gas_mode_set(1, function(error) {
+    thingy.gas_mode_set(1, (error) => {
       if (error) {
-        console.log('Gas sensor configure! ' + error);
+        console.log('Failed to configure gas sensor:', error);
       }
     });
 
-    thingy.temperature_enable(function(error) {
-      console.log('Temperature sensor started! ' + ((error) ? error : ''));
+    thingy.temperature_enable((error) => {
+      if (error) {
+        console.log('Failed to enable temperature sensor:', error);
+      }
     });
-    thingy.pressure_enable(function(error) {
-      console.log('Pressure sensor started! ' + ((error) ? error : ''));
+    thingy.pressure_enable((error) => {
+      if (error) {
+        console.log('Failed to enable pressure sensor:', error);
+      }
     });
-    thingy.humidity_enable(function(error) {
-      console.log('Humidity sensor started! ' + ((error) ? error : ''));
+    thingy.humidity_enable((error) => {
+      if (error) {
+        console.log('Failed to enable humidity sensor:', error);
+      }
     });
-    thingy.color_enable(function(error) {
-      console.log('Color sensor started! ' + ((error) ? error : ''));
+    thingy.color_enable((error) => {
+      if (error) {
+        console.log('Failed to enable color sensor:', error);
+      }
     });
-    thingy.gas_enable(function(error) {
-      console.log('Gas sensor started! ' + ((error) ? error : ''));
+    thingy.gas_enable((error) => {
+      if (error) {
+        console.log('Failed to enable gas sensor:', error);
+      }
     });
-    thingy.button_enable(function(error) {
-      console.log('Button started! ' + ((error) ? error : ''));
+    thingy.button_enable((error) => {
+      if (error) {
+        console.log('Failed to enable button:', error);
+      }
     });
-    thingy.notifyBatteryLevel(function(error) {
-      console.log('Battery Level Notifications enabled! ' + ((error) ? error : ''));
+    thingy.notifyBatteryLevel((error) => {
+      if (error) {
+        console.log('Failed to enable battery level notifications:', error);
+      }
     });
   });
 
-  thing.addAvailableEvent('pressed', {
-    description: 'Button pressed'
+  thing.addAvailableEvent('buttonPressed', {
+    description: 'Button pressed',
   });
-  thing.addAvailableEvent('released', {
-    description: 'Button released'
+  thing.addAvailableEvent('buttonReleased', {
+    description: 'Button released',
   });
 
-  const server = new WebThingServer(thing, 'thingy52', 8888);
+  const server = new WebThingServer(new SingleThing(thing), 8888);
   server.start();
 }
 
